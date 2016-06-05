636cb512a18356fbc0f96023dca732bc29c2cfd0
Jonathan Germond <jonathan@saagie.com>
2016-06-05 09:50:42 +0200

    Add SaagieHiringBundle

---
+++ src/SaagieHiringBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SaagieHiringBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('SaagieHiringBundle:Default:index.html.twig');
+    }
+}
---
+++ src/SaagieHiringBundle/SaagieHiringBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SaagieHiringBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SaagieHiringBundle extends Bundle
+{
+}
